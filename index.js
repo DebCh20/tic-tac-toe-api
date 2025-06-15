510c73ecb3b0bf5147954cfc3fb5155a9cf7eb77
Debasish <101311620+DebCh20@users.noreply.github.com>
2025-06-15 10:21:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+require('dotenv').config()
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Adjust as per your React app's origin
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("move", (data) => {
+    // Broadcast the move to all other players
+    console.log('data received at server mark',data.showMark);
+    console.log('data received at server state',data.gameState);
+    
+    socket.emit("move", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Socket.IO server running on port ${process.env.PORT}`);
+});
